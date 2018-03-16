452103667c7b2704c757c6e76a4a319df07614b4
QuyDuong <duongminhquyltv>
2018-03-16 23:06:52 +0700

    Basic Project

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+	/*
+	* Declare global variable
+	**/
+	
+	// Information connect database.
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASS','');
+	define('DATABASE','smallVinaphone');
+	
+	
+?>

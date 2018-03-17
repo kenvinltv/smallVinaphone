689fbe01d7d57701e69eecd6dc459fb958c4d41a
QuyDuong <duongminhquyltv>
2018-03-17 07:18:02 +0700

    KenVin Duong

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include_once 'config.php' ?>
 <html>
 <head>
+    <!-- http://themes.ladipage.vn/#/preview-ladipage?ladi=594dca37ab9d34daa02cf337 -->
     <meta charset="utf-8">
     <base href="http://localhost/smallVinaphone/">
     <title><?php echo title; ?></title>
@@ -9,7 +10,11 @@
     <link rel="shortcut icon" type="image/ico" href="favicon.ico">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="asset/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="asset/css/normalize.css">
     <link rel="stylesheet" href="asset/css/style.css">
     
@@ -31,10 +36,211 @@
                 </div>                
             </div>
         </section>
+        <section class="mobile-internet">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 title">
+                        <h2 class="text-center"><span>Gói cước MI</span></h2>
+                        <p class="title-description text-center">Các gói cước Mobile Internet do VinaPhone cung cấp.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="box">
+                             <div class="box-thumbnails">
+                                <span><i class="fas fa-dollar-sign"></i></span>
+                             </div>
+                             <div class="box-content">
+                                <h4><a href="" title="">Bảng Giá Cước</a></h4>
+                                <p>Bảng giá cước dịch vụ mobile internet.</p>
+                             </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="box">
+                             <div class="box-thumbnails">
+                                <span><i class="fas fa-clipboard-list"></i></span>
+                             </div>
+                             <div class="box-content">
+                                <h4><a href="" title="">Bảng Qui Đổi</a></h4>
+                                <p>Sử dụng dịch vụ & bảng qui đổi lưu lượng gói cuóc.</p>
+                             </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="box">
+                             <div class="box-thumbnails">
+                                <span><i class="fas fa-sun"></i></span>                         
+                             </div>
+                             <div class="box-content">
+                                <h4><a href="" title="">Đăng ký</a></h4>
+                                <p>Đăng ký gói cước qua SMS.</p>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="price-mobile-internet">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 title">
+                        <h2 class="text-center"><span>BẢNG GIÁ CƯỚC DỊCH VỤ MOBILE INTERNET</span></h2>                        
+                    </div>
+                </div>
+                
+                <div class="price-slide owl-carousel">
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <span class="thumbnails"><i class="fas fa-cubes"></i></span>
+                        <p class="title-slide"><a href="#" data-toggle="modal" data-target="#myModal">GÓI CƯỚC KHÔNG CÓ DUNG LƯỢNG MIỄN PHÍ</a></p>
+                        <div class="description-slide">
+                            <p>(Nhân đôi dung lượng data hàng tháng từ 20/11/2017 đến 25/2/2018)</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="note">
+                    <div class="box-description">
+                            <p class="note-text">Lưu ý:</p>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="box-content">
+                                        <h4><a href="" title="">Thời gian sử dụng</a></h4>
+                                        <ol>
+                                            <li>Đối với gói cước M0 là vô thời hạn.</li>
+                                            <li>Đối với các gói cước M10, M25, M50, M120, MAX, MAXS, MAX100, MAX200 là 30 ngày.</li>
+                                        </ol>
+                                    </div>                                
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="box-content">
+                                        <h4><a href="" title="">Cước lưu lượng vượt gói:</a></h4>
+                                        <ol>
+                                            <li>Đối với gói cước M0 là 75đ/50KB.</li>
+                                            <li>Đối với các gói cước M10, M25, M50, M120 là 25đ/50KB.</li>
+                                            <li>Đối với các gói cước MAX, MAXS, MAX100, MAX200 là miễn phí</li>
+                                        </ol>
+                                    </div>                                
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>            
+        </section>    
+
+
+        <section class="mobile-internet ezCom">
+            <div class="container">                
+                <div class="row">
+                    <div class="col-xs-4 title">
+                        <h2><span>EzCom</span></h2>
+                        <p class="title-description">ezCom là dịch vụ truy cập Internet từ máy tính bảng.</p>
+                    </div>
+                    <div class="col-xs-8">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="box">
+                                     <div class="box-thumbnails">
+                                        <span><i class="fas fa-dollar-sign"></i></span>
+                                     </div>
+                                     <div class="box-content">
+                                        <h4><a href="" title="">Bảng Giá Cước</a></h4>
+                                        <p>Bảng giá cước dịch vụ EzCom.</p>
+                                     </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="box">
+                                     <div class="box-thumbnails">
+                                        <span><i class="fas fa-sun"></i></span>                         
+                                     </div>
+                                     <div class="box-content">
+                                        <h4><a href="" title="">Đăng ký</a></h4>
+                                        <p>Đăng ký gói cước qua SMS.</p>
+                                     </div>
+                                </div>
+                            </div>                           
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="box">
+                                     <div class="box-thumbnails">
+                                        <span><i class="fas fa-exchange-alt"></i></span>
+                                     </div>
+                                     <div class="box-content">
+                                        <h4><a href="" title="">Chuyển sim</a></h4>
+                                        <p>Chuyển SIM thường qua SIM eZcom.</p>
+                                     </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="box">
+                                     <div class="box-thumbnails">
+                                        <span><i class="fab fa-monero"></i></span>                         
+                                     </div>
+                                     <div class="box-content">
+                                        <h4><a href="" title="">Thanh toán</a></h4>
+                                        <p>Cách thức thanh toán.</p>
+                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
     <!--.wrapper-->
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="asset/js/owl.carousel.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="asset/js/custom.js"></script>
 </body>
